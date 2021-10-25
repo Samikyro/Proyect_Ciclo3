f38bfe71edff9405e9eee680a1b6eadfa90a2096
Samikyro <sandra.quitian28@gmail.com>
2021-10-25 00:06:41 -0500

    conexion con mongo

---
+++ nuevo/routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {
+  getVentasContorller,
+  createVentasController,
+} = require("../controllers/ventas.controller");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// routes ventas
+router.get("/ventas", getVentasContorller);
+router.post("/ventas", createVentasController);
+
+module.exports = router;
